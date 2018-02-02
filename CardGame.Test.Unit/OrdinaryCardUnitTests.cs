8d8525e3145224e11cc8716feec45a2a7829687c
jonasna <jonasna93@gmail.com>
2018-02-02 16:33:37 +0100

    Added working test project and added initial test for ordinary card

---
+++ CardGame.Test.Unit/OrdinaryCardUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CardGame.Test.Unit
+{
+    [TestFixture]
+    public class OrdinaryCardUnitTests
+    {
+        [Test]
+        public void Constructor_SetNumberTo5_NumberIsCorrect()
+        {
+            var uut = new OrdinaryCard(new RedColor(), 5);
+            Assert.That(uut.Number, Is.EqualTo(5));
+        }
+    }
+}
