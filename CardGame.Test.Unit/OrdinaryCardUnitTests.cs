bcf6d5bbcf8772862a3e97f231508957d72621b4
jonasna <jonasna93@gmail.com>
2018-02-02 18:01:16 +0100

    Added remaining tests for ordinary card

--- CardGame.Test.Unit/OrdinaryCardUnitTests.cs
+++ CardGame.Test.Unit/OrdinaryCardUnitTests.cs
@@ -11,10 +11,38 @@ namespace CardGame.Test.Unit
     public class OrdinaryCardUnitTests
     {
         [Test]
-        public void Constructor_SetNumberTo5_NumberIsCorrect()
+        public void Constructor_SetNumberTo5_NumberIs5()
         {
             var uut = new OrdinaryCard(new RedColor(), 5);
             Assert.That(uut.Number, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Constructor_SetNumberTo9_NumberIs1()
+        {
+            var uut = new OrdinaryCard(new RedColor(), 9);
+            Assert.That(uut.Number, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Constructor_SetNumberToNegative_NumberIs1()
+        {
+            var uut = new OrdinaryCard(new RedColor(), -500);
+            Assert.That(uut.Number, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ToString_RedCardFive_FormatIsCorrect()
+        {
+            var uut = new OrdinaryCard(new RedColor(), 5);
+            Assert.That(uut.ToString(), Is.EqualTo("Red 5"));
+        }
+
+        [Test]
+        public void GetValue_RedCardFive_ReturnIs5()
+        {
+            var uut = new OrdinaryCard(new RedColor(), 5);
+            Assert.That(uut.Value, Is.EqualTo(5));
         }
     }
 }
